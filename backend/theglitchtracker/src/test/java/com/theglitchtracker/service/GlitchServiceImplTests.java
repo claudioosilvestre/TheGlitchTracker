efb42398e8536e8cce755a3342f2efc7d15b9fb7
Cláudio Silvestre <claudioosilvestre@hotmail.com>
2026-07-14 08:44:04 +0100

    add updateGlitch tests at GlitchServiceImplTests

--- backend/theglitchtracker/src/test/java/com/theglitchtracker/service/GlitchServiceImplTests.java
+++ backend/theglitchtracker/src/test/java/com/theglitchtracker/service/GlitchServiceImplTests.java
@@ -174,4 +174,69 @@ public class GlitchServiceImplTests {
         assertEquals(GlitchPriority.GLITCH, result.getGlitchPriority());
         verify(glitchRepository).save(glitch);
     }
+
+    @Test
+    void updateGlitch_withNegativeIdShouldThrowException() {
+
+        Glitch glitch = new Glitch();
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> glitchService.updateGlitch(-1, glitch)
+        );
+        assertEquals("Glitch id must be positive", exception.getMessage());
+    }
+
+    @Test
+    void updateGlitch_withNullGlitchShouldThrowException() {
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> glitchService.updateGlitch(1, null)
+        );
+        assertEquals("Glitch can not be null", exception.getMessage());
+    }
+
+    @Test
+    void updateGlitch_shouldReturnGlitchUpdated() {
+
+        Glitch glitch = new Glitch();
+        glitch.setId(1);
+        glitch.setTitle("test");
+        glitch.setGlitchStatus(GlitchStatus.IDENTIFIED);
+        glitch.setGlitchPriority(GlitchPriority.GLITCH);
+
+        when(glitchRepository.findById(1)).thenReturn(Optional.of(glitch));
+
+        Glitch updatedGlitch = new Glitch();
+        updatedGlitch.setTitle("updatedTest");
+        updatedGlitch.setGlitchPriority(GlitchPriority.DEJA_VU);
+        updatedGlitch.setGlitchStatus(GlitchStatus.SYSTEM_FIXED);
+
+        when(glitchRepository.save(glitch)).thenReturn(glitch);
+
+        Glitch result = glitchService.updateGlitch(1, updatedGlitch);
+
+        assertNotNull(result);
+        assertEquals(1, result.getId());
+        assertEquals("updatedTest", result.getTitle());
+        assertEquals(GlitchPriority.DEJA_VU, result.getGlitchPriority());
+        assertEquals(GlitchStatus.SYSTEM_FIXED, result.getGlitchStatus());
+        verify(glitchRepository).findById(1);
+        verify(glitchRepository).save(glitch);
+    }
+
+    @Test
+    void updateGlitch_shouldThrowExceptionIfGlitchNotFound() {
+
+        Glitch glitch = new Glitch();
+
+        when(glitchRepository.findById(5)).thenReturn(Optional.empty());
+
+        TheGlitchTrackerException exception = assertThrows(
+                TheGlitchTrackerException.class,
+                () -> glitchService.updateGlitch(5, glitch)
+        );
+        assertEquals("Glitch does not exist", exception.getMessage());
+    }
 }
