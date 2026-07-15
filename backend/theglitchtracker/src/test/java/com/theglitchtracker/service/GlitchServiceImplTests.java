fa312bf08376220e5a6806e5d7b2bed2a1c428ca
Cláudio Silvestre <claudioosilvestre@hotmail.com>
2026-07-15 07:37:14 +0100

    add tests updateStatus and addUserToGlitch at GlicthServiceImplTests

--- backend/theglitchtracker/src/test/java/com/theglitchtracker/service/GlitchServiceImplTests.java
+++ backend/theglitchtracker/src/test/java/com/theglitchtracker/service/GlitchServiceImplTests.java
@@ -4,6 +4,7 @@ import com.theglitchtracker.exception.TheGlitchTrackerException;
 import com.theglitchtracker.model.Glitch;
 import com.theglitchtracker.model.GlitchPriority;
 import com.theglitchtracker.model.GlitchStatus;
+import com.theglitchtracker.model.User;
 import com.theglitchtracker.repository.GlitchRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -294,5 +296,79 @@ public class GlitchServiceImplTests {
                 () -> glitchService.updateStatus(5, GlitchStatus.BENDING_THE_RULES)
         );
         assertEquals("Glitch does not exist", exception.getMessage());
+    }
+
+    @Test
+    void updateStatus_shouldSetResolvedAtNowIfSystemIsFixed() {
+
+        Glitch glitch = new Glitch();
+        glitch.setId(1);
+        glitch.setTitle("test");
+        glitch.setGlitchStatus(GlitchStatus.BENDING_THE_RULES);
+
+        when(glitchRepository.findById(1)).thenReturn(Optional.of(glitch));
+        when(glitchRepository.save(glitch)).thenReturn(glitch);
+
+        Glitch result = glitchService.updateStatus(1, GlitchStatus.SYSTEM_FIXED);
+
+        assertNotNull(result);
+        assertNotNull(glitch.getResolvedAt());
+        assertEquals(1, result.getId());
+        assertEquals("test", result.getTitle());
+        assertEquals(GlitchStatus.SYSTEM_FIXED, result.getGlitchStatus());
+        verify(glitchRepository).findById(1);
+        verify(glitchRepository).save(glitch);
+    }
+
+    @Test
+    void updateStatus_shouldSetResolvedAtNullIfTheSystemIsNotFixed() {
+
+        Glitch glitch = new Glitch();
+        glitch.setId(1);
+        glitch.setTitle("test");
+        glitch.setGlitchStatus(GlitchStatus.SYSTEM_FIXED);
+        glitch.setResolvedAt(LocalDateTime.now());
+
+        when(glitchRepository.findById(1)).thenReturn(Optional.of(glitch));
+        when(glitchRepository.save(glitch)).thenReturn(glitch);
+
+        Glitch result = glitchService.updateStatus(1, GlitchStatus.IDENTIFIED);
+
+        assertNotNull(result);
+        assertEquals(1, result.getId());
+        assertEquals("test", result.getTitle());
+        assertEquals(GlitchStatus.IDENTIFIED, result.getGlitchStatus());
+        assertNull(result.getResolvedAt());
+        verify(glitchRepository).findById(1);
+        verify(glitchRepository).save(glitch);
+    }
+
+    @Test
+    void addUserToGlitch_shouldReturnGlitchWithUser() {
+
+        Glitch glitch = new Glitch();
+        glitch.setId(1);
+        glitch.setTitle("test");
+        glitch.setGlitchStatus(GlitchStatus.IDENTIFIED);
+
+        User user = new User();
+        user.setId(2);
+        user.setName("nameTest");
+
+        when(glitchRepository.findById(1)).thenReturn(Optional.of(glitch));
+        when(userService.findById(2)).thenReturn(user);
+        when(glitchRepository.save(glitch)).thenReturn(glitch);
+
+        Glitch result = glitchService.addUserToGlitch(1, 2);
+
+        assertNotNull(result);
+        assertEquals(1, result.getId());
+        assertEquals("test", result.getTitle());
+        assertEquals(GlitchStatus.IDENTIFIED, result.getGlitchStatus());
+        assertEquals(2, result.getUser().getId());
+        assertEquals("nameTest", result.getUser().getName());
+        verify(glitchRepository).findById(1);
+        verify(userService).findById(2);
+        verify(glitchRepository).save(glitch);
     }
 }
